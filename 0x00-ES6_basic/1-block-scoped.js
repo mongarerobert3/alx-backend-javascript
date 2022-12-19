f5365bef69f5baf12bc1115adef387fe5b254562
mongarerobert3 <mongarerobert3@gmail.com>
2022-12-19 16:07:00 +0300

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-const task = false;
-const task2 = true;
+  const task = false;
+  const task2 = true;
 
-if (trueOrFalse) {
-  const task = true; // eslint-disable-line no-unused-vars
-  const task2 = false; // eslint-disable-line no-unused-vars
-}
+  if (trueOrFalse) {
+    // eslint-disable-next-line
+    const task = true;
+    // eslint-disable-next-line
+    const task2 = false;
+  }
 
-return [task, task2];
+  return [task, task2];
 }

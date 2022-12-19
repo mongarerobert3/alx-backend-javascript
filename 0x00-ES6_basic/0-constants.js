26f8da62c58b65659dd1c69a3d10116a7c4d603d
mongarerobert3 <mongarerobert3@gmail.com>
2022-12-19 15:09:44 +0300

    Const or let?

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,14 @@
-export function taskFirst() {
+taskFirst => {
     const task = 'I prefer const when I can.';
     return task;
   }
   
-  export function getLast() {
+   function getLast() {
     return ' is okay';
   }
-  
-  export function taskNext() {
+taskNext => {
     let combination = 'But sometimes let';
     combination += getLast();
   
     return combination;
   }
-  

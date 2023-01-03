64791f32a21708f18afbf89d3c6f35f0ed6b4cd6
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-03 22:03:11 +0300

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,11 @@
-export default function getListStudents() {
-  return new Array(
-    {
-      id: 1,
-      firstname: 'Guillaume',
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      firstname: 'James',
-      location: 'Columbia',
-    },
-    {
-      id: 5,
-      firstname: 'Serena',
-      location: 'San Francisco',
-    },
-  );
-}
+const getListStudents = () => {
+  const students = [];
+  const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  students.push(student1, student2, student3);
+
+  return students;
+};
+
+export default getListStudents;

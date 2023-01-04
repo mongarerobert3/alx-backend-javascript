b88a428087c9a446a7d1d9df093968f465e08943
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-04 15:58:13 +0300

    Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+export interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [index:string]: any;
+  }
+  

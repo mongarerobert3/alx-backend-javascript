e38ea9854902c628f349d4d569069f99afd19a1a
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-23 11:29:25 +0300

    Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Send the response body "Hello World"
+  res.end('Hello Holberton School!\n');
+});
+
+// Listen on port 8080, IP defaults to 127.0.0.1
+app.listen(1245);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:8080/');
+
+module.exports = app;

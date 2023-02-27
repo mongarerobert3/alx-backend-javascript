8501506cc84a6d497406eefd7c50e02571b18574
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-27 21:42:31 +0300

    Node Redis client and basic operations

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -18,3 +18,17 @@ app.listen(PORT, HOST, () => {
 });
 
 module.exports = app;
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Send the response body "Hello World"
+  res.end('Hello Holberton School!\n');
+});
+
+// Listen on port 8080, IP defaults to 127.0.0.1
+app.listen(1245);
+
+module.exports = app;
